c5fd2edd299a1bbfca5bd1b86195928a8803f41d
thaise <thaisevilaca2013@gmail.com>
2017-08-17 13:38:34 -0300

    adicionando java no Main

--- app/src/main/java/com/example/samsung/listview/MainActivity.java
+++ app/src/main/java/com/example/samsung/listview/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.samsung.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lista = (ListView) findViewById(R.id.lvEquipes);
+
+            ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
+                R.array.equipes, android.R.layout.simple_list_item_1);
+                lista.setAdapter(adapter);
     }
 }
