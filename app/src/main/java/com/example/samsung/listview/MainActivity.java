ca40e82e5e732af3cf24969068d8d81eac896d79
thaise <thaisevilaca2013@gmail.com>
2017-08-17 14:20:43 -0300

    Criando a classe MainActivity2 e alterando cod java

--- app/src/main/java/com/example/samsung/listview/MainActivity.java
+++ app/src/main/java/com/example/samsung/listview/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.samsung.listview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import adapter.EscolaAdapter;
+import model.Escola;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +21,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView lista = (ListView) findViewById(R.id.lvEquipes);
+        ListView lista = (ListView) findViewById(R.id.lvEscolas);
+        final ArrayList<Escola> escolas = adicionarEscolas();
+        ArrayAdapter adapter = new EscolaAdapter(this, escolas);
+        lista.setAdapter(adapter);
 
-            ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
-                R.array.equipes, android.R.layout.simple_list_item_1);
-                lista.setAdapter(adapter);
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView,
+                                    View view, int i, long l) {
+                Toast.makeText(getBaseContext(), "Escola: "+escolas.get(i).getNome(),
+                        Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("nome", escolas.get(i).getNome());
+                startActivity(intent);
     }
+            private ArrayList<Escola> adicionarEscolas() {
+                ArrayList<Escola> escolas = new ArrayList<Escola>();
+                Escola e = new Escola("Colégio Estadual Atheneu Sergipense",
+                        "Rua Pacatuba S/N", R.drawable.escola1);
+                escolas.add(e);
+                e = new Escola("Escola Estadual General Siqueira",
+                        "Rua Sergipe S/N", R.drawable.escola1);
+                escolas.add(e);
+                e = new Escola("Escola Tobias Barreto",
+                        "Av. Otoniel Dórea", R.drawable.escola1);
+                escolas.add(e);
+                e = new Escola("Colegio Estadual Prof G Rollemberg Leite",
+                        "R. Franklin de Campos Sobral, 1675", R.drawable.escola1);
+                escolas.add(e);
+                e = new Escola("Escola Estadual General Siqueira",
+                        "Rua Sergipe S/N", R.drawable.escola1);
+                escolas.add(e);
+                e = new Escola("Escola Tobias Barreto",
+                        "Av. Otoniel Dórea", R.drawable.escola1);
+                escolas.add(e);
+                e = new Escola("Colegio Estadual Prof G Rollemberg Leite",
+                        "R. Franklin de Campos Sobral, 1675", R.drawable.escola1);
+                escolas.add(e);
+                return escolas;
+            }
 }
---
+++ app/src/main/java/com/example/samsung/listview/MainActivity2.java
@@ -0,0 +1,23 @@
+package com.example.samsung.listview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by Samsung on 17/08/2017.
+ */
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent intent = getIntent();
+        String parametro = (String) intent.getSerializableExtra("nome");
+        TextView nome = (TextView) findViewById(R.id.tvNome);
+        nome.setText(parametro);
+    }
+}
